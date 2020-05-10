2781aea7768f27dd3b89945918aea493d91496bb
Max Robinson <maxdrob1@gmail.com>
2020-05-10 13:49:46 -0600

    Added player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject ToFollow;
+    public float CameraMoveSpeed = 5f;
+
+    Vector3 newCameraPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 toFollowPosition = ToFollow.transform.position;
+        toFollowPosition.z = transform.position.z;
+
+        Vector3 cameraMoveDir = (toFollowPosition - transform.position).normalized;
+        float distance = Vector3.Distance(toFollowPosition, transform.position);
+
+        if (distance > 0)
+        {
+            Vector3 newCameraPosition = transform.position + cameraMoveDir * distance * CameraMoveSpeed * Time.deltaTime;
+
+            float distanceAfterMoving = Vector3.Distance(newCameraPosition, toFollowPosition);
+            if (distanceAfterMoving > distance)
+            {
+                // we overshot the target
+                newCameraPosition = toFollowPosition;
+            }
+
+            transform.position = newCameraPosition;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal = 0;
+    float vertical = 0;
+    float look_angle = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        float x1 = mousePosition.x;
+        float x2 = rigidbody2d.position.x;
+        float y1 = mousePosition.y;
+        float y2 = rigidbody2d.position.y;
+        float xDiff = x1 - x2;
+        float yDiff = y1 - y2;
+        look_angle = (float)(Mathf.Atan2(yDiff, xDiff) * Mathf.Rad2Deg) + 90;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 move = new Vector2(horizontal, vertical);
+        Vector2 new_position = rigidbody2d.position + move * speed * Time.fixedDeltaTime;
+
+
+        rigidbody2d.MovePosition(new_position);
+        rigidbody2d.MoveRotation(look_angle);
+        //rigidbody2d.transform.center
+        //transform.position = new_position;
+        //transform.rotation = Quaternion.Euler(0, 0, look_angle);
+    }
+}
