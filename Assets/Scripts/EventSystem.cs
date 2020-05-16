5347622191a42dc0a6f41493ce93e2237f2631c6
Kolby Nottingham <kolbytn@gmail.com>
2020-05-15 17:11:32 -0700

    Open close menu

---
+++ Assets/Scripts/EventSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystem : MonoBehaviour
+{
+    public GameObject gameMenu;
+
+    private bool isVisible = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isVisible = !isVisible;
+            if (isVisible)
+                Debug.Log("Is Visible");
+            else
+                Debug.Log("Is not Visible");
+        }
+
+        gameMenu.SetActive(isVisible);
+    }
+}
