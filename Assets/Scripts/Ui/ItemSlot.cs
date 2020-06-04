9ac799ab856fb9497e1d1ca1c7e577fd196150e7
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2020-06-04 14:24:56 -0600

    Added a basic hotbar u9i

---
+++ Assets/Scripts/Ui/ItemSlot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+
+    public Image ItemIcon;
+
+    void Start() {
+        // ItemIcon.enabled = false;
+    }
+
+    public void SetIcon(Sprite sprite) {
+        ItemIcon.enabled = true;
+        ItemIcon.sprite = sprite;
+    }
+
+    public void ClearIcon() {
+        ItemIcon.sprite = null;
+        ItemIcon.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Ui/UiHotbar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiHotbar : MonoBehaviour
+{
+
+    ItemSlot[] slots;
+
+    // Start is called before the first frame update
+    void Start() {
+        slots = GetComponentsInChildren<ItemSlot>();
+        Debug.Log("chicken");
+
+        foreach(ItemSlot slot in slots) {
+            Debug.Log("slot");
+            slot.SetIcon(WorldResources.BerriesIcon);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
